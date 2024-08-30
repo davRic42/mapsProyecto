20cb33b813b1eb976fd28cdedbdcefe290a43f26
sergio2732 <sergioparra1227@gmail.com>
2024-08-29 19:50:55 -0500

    arreglo de visualización de mapa
    Se arreglo el inconveniente de no mostrar el mapa en la pantalla

--- Logica/script.js
+++ Logica/script.js
@@ -80,14 +80,3 @@ function deleteTutor() {
     })
     .catch(error => console.error('Error:', error));
 }
-function iniciarMap(){
-    var coord = {lat:-34.5956145 ,lng: -58.4431949};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
-}
